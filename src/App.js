684275e255f9a1145312708e5eacefecbea989f4
Modrogan,George <George.Modrogan@Cerner.com>
2021-07-15 22:25:49 +0300

    some changes v2...

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MGW test
+          MGW test v2
         </p>
       </header>
     </div>
